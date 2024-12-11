f17cd17f263b73c6fc5bfbfd55616c5d3d4c52a8
donank <11donank22@gmail.com>
2024-12-11 22:57:24 +0000

    Simplify regex mess

--- challenge 2/solution.sql
+++ challenge 2/solution.sql
@@ -7,15 +7,6 @@ WITH letters_union AS
 (
     SELECT STRING_AGG(CHR(lu.value),'') 
     FROM letters_union lu
-    WHERE CHR(lu.value) ~* '^.*[A-Za-z].*$'
-    OR CHR(lu.value) ~* ' '
-    OR CHR(lu.value) ~* '^.*\..*$'
-    OR CHR(lu.value) ~* '!'
-    OR CHR(lu.value) ~* ','
-    OR CHR(lu.value) ~* '-'
-    OR CHR(lu.value) ~* '^.*\?$'
-    OR CHR(lu.value) ~* ':'
-    OR CHR(lu.value) ~* ';'
-    OR CHR(lu.value) ~* '"'
+    WHERE CHR(lu.value) ~* '[a-z !",\(\)\-\.:;?]'
 )
 SELECT * FROM aggregated_string;
